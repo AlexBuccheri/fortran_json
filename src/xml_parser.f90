08b3e4af6f7b04cee58ac46790e1c3adfb965559
Alex Buccheri <alexanderbuccheri@googlemail.com>
2020-10-19 19:59:53 +0100

    Added links to XML library for fortran

--- src/xml_parser.f90
+++ src/xml_parser.f90
@@ -1,7 +1,11 @@
 module xml_parser
   implicit none
   private
-  ! Module is a stab - needs to use a library
+  ! Module is a stub - needs an externala library, for example:
+  !  https://github.com/andreww/fox
+  !  https://github.com/andreww/fox/blob/master/examples/wxml_example.f90
+  !  http://homepages.see.leeds.ac.uk/~earawa/FoX/DoX/FoX_DoX.html#FoX_wxml
+  
   ! Write overloads for initialise, destroy, writing to file and
   ! putting each data type into the XML tree
 end module xml_parser
